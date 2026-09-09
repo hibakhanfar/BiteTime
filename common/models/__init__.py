149e0d2e4e637cc03f6e14e1dd86dd2d3ed8d6d1
Hiba Ibrahim Khanfar <hibaibraheem140@gmail.com>
2026-09-09 15:22:41 +0300

    user_serializer+ components

---
+++ common/models/__init__.py
@@ -0,0 +1,6 @@
+from .User import User
+from .Order import Order
+from .OrderItem import OrderItem
+from .MenuItem import MenuItem
+
+__all__ = ["User", "Order", "OrderItem", "MenuItem"]
---
+++ common/serializers/UserSerializer.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+from django.contrib.auth.password_validation import validate_password
+from common.models.User import User
+from common.components import UserService
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("username", "email", "password")
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def validate_password(self, value):
+        validate_password(value)
+        return value
+
+    def create(self, validated_data):
+        return UserService.register_user(
+            username=validated_data["username"],
+            email=validated_data["email"],
+            password=validated_data["password"],
+        )
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("id", "username", "email")
+        read_only_fields = ("id", "username", "email")
