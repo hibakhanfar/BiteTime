f04f643bb38e6e16bddd789fd539b1e6590b13d6
Hiba Ibrahim Khanfar <hibaibraheem140@gmail.com>
2026-09-14 09:55:15 +0300

    create menu

--- common/serializers/MenuItemSerializer.py
+++ common/serializers/MenuItemSerializer.py
@@ -5,4 +5,10 @@ from common.models import MenuItem
 class MenuItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = MenuItem
+        fields = ("id", "name", "price", "estimated_prep_minutes", "image", "allergen_tags")
+
+
+class MenuItemCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MenuItem
         fields = ("name", "price", "estimated_prep_minutes", "image", "allergen_tags")
--- common/views/MenuItemListView.py
+++ common/views/MenuItemListView.py
@@ -1,21 +1,36 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListCreateAPIView
 from rest_framework.permissions import IsAuthenticated
 from common.responses import api_response
+from common.permissions import HasRole
 from common.models import MenuItem
-from common.serializers.MenuItemSerializer import MenuItemSerializer
+from common.serializers.MenuItemSerializer import MenuItemSerializer, MenuItemCreateSerializer
 
 
-class MenuItemListView(ListAPIView):
-    permission_classes = [IsAuthenticated]
+class MenuItemListView(ListCreateAPIView):
     queryset = MenuItem.objects.filter(is_available=True)
-    serializer_class = MenuItemSerializer
+
+    def get_permissions(self):
+        if self.request.method == "POST":
+            self.allowed_roles = ["CHEF"]
+            return [HasRole()]
+        return [IsAuthenticated()]
+
+    def get_serializer_class(self):
+        if self.request.method == "POST":
+            return MenuItemCreateSerializer
+        return MenuItemSerializer
 
     def list(self, request, *args, **kwargs):
         queryset = self.get_queryset()
         serializer = self.get_serializer(queryset, many=True)
-
         return api_response(
-            status_code=200,
-            message="Menu retrieved successfully",
-            data=serializer.data,
+            status_code=200, message="Menu retrieved successfully", data=serializer.data
+        )
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return api_response(
+            status_code=201, message="Menu created successfully", data=serializer.data
         )
