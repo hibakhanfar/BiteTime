ab08fb76c5c3de44c0ecdd8acb21a2bdd4fa96c4
Hiba Ibrahim Khanfar <hibaibraheem140@gmail.com>
2026-09-24 00:49:07 +0300

    set up Celery worker and beat for order state updates

--- core/settings/base.py
+++ core/settings/base.py
@@ -186,3 +186,11 @@ CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
 CELERY_ACCEPT_CONTENT = ["json"]
 CELERY_TASK_SERIALIZER = "json"
 CELERY_TIMEZONE = "UTC"
+
+
+CELERY_BEAT_SCHEDULE = {
+    "check-ready-orders-every-30-seconds": {
+        "task": "common.tasks.check_and_update_ready_orders",
+        "schedule": 30.0,
+    },
+}
